0b9f465cfc2b0d87c686793af386646dc97510b7
21bca44 <97145857+21bca44@users.noreply.github.com>
2022-03-09 10:11:32 +0530

    Delete prime.py

--- prime.py
+++
@@ -1,9 +0,0 @@
-a=int(input("enter any number"))
-b=0
-for i in a:
-    if(b%i==0):
-        print("this is not prime number")
-        break
-    if(i==b):
-        print("this is prime number")
-        
